26b22356ea890e0036316a9ae497be4823019d90
supragya27 <supragya.chauhan001@gmail.com>
2020-06-04 16:35:40 +0530

    updated

--- blog-website/src/components/Hero/index.js
+++ blog-website/src/components/Hero/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from '../UI/Card'
+import Logo from '../Logo'
 
 function Hero() {
     return (
         <div>
-            
+           <Card>
+           <Logo/>
+           </Card>
         </div>
     )
 }
---
+++ blog-website/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo() {
+    return (
+        <div className="logo">
+            <a href="#">
+            Supragya Chauhan
+            </a>
+        </div>
+    )
+}
+
+export default Logo
